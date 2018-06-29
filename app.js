2ea6753d0a7bb0bf160c8cfea6e6f98fdc8255db
Christian Lowe <christglowe@gmail.com>
2018-06-29 14:11:49 -0600

    Got basic face detection to work in the Python file. Now what's needed is an implementation of the node child processes to run the Python file after recieving the image from the client

--- app.js
+++ app.js
@@ -18,22 +18,7 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 
 app.get("/", (req, res) => {
-  res.render("login")
-
-  let txt = fs.readFile('Biddr.png', (err, data) => {
-    if (err) {
-       throw err;
-     }
-     const { StringDecoder } = require('string_decoder');
-     const decoder = new StringDecoder('base64');
-
-     const cent = Buffer.from(data);
-     console.log(decoder.write(cent));
-
-     fs.writeFile('Biddr.txt', data, 'base64', (err) => {if (err) throw err})
-     console.log(data);
-  })
-
+  res.render("login");
 })
 
 app.post('/',function(req,res){
@@ -45,7 +30,7 @@ app.post('/',function(req,res){
     let data = body;
     data = new Buffer(data, 'base64');
     console.log(data)
-    fs.writeFile('./uploads/face69.png', data, (err) => {
+    fs.writeFile('./uploads/face69.jpg', data, (err) => {
       if (err) throw err;
       console.log('The file has been saved!');
     });
