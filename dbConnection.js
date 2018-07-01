94a5cd3a71adbab645994e30729c6b9170090890
christianlowe <christglowe@gmail.com>
2018-07-01 14:36:06 -0600

    got the dotenv package to work. The error was occurring because I was imporprerly importing the package into dbConnection.js. To work properly one should just write 'require(dotenv).config()' and not 'dotenv = require(dotenv).config()' as I had previously thought

--- dbConnection.js
+++ dbConnection.js
@@ -1,16 +1,24 @@
 const Sequelize = require("sequelize");
-
+require("dotenv").config()
 const sequelize = new Sequelize(process.env.DB, process.env.USER, process.env.PASSWORD, {
   host: process.env.HOST,
-  dialect: "postgres"
+  dialect: "postgres",
+  operatorsAliases: false,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
 })
 
 
 const User = sequelize.define("User", {
-  user_id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true,}
-  userName: Sequelize.STRING,
-  firstName: Sequelize.STRING,
-  lastName: Sequelize.STRING,
-  email: Sequelize.STRING,
-
+  user_id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+  userName: { type: Sequelize.STRING, allowNull: false },
+  firstName: { type: Sequelize.STRING, allowNull: false },
+  lastName: { type: Sequelize.STRING, allowNull: false },
+  email: { type: Sequelize.STRING, allowNull: false },
+  password: { type: Sequelize.STRING, allowNull: false },
+  verification_image: { type: Sequelize.STRING, allowNull: true }
 })
